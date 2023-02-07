e65393a76715f9fd47cdbdc0fe978e361514f5c9
lauths12 <l.carretero@uniandes.edu.co>
2023-02-07 15:35:37 -0500

    parser jeje

---
+++ Parser/parser.py
@@ -0,0 +1,19 @@
+import ply.yacc as yacc
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
